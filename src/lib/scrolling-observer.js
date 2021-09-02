10bdf0f2de7139677548e91be1394c60e0f57529
dimitarkolev <dk@almero.bg>
2021-09-02 11:34:52 +0000

    Overwrite

--- src/lib/scrolling-observer.js
+++ src/lib/scrolling-observer.js
@@ -1,129 +1,48 @@
-import { onBeforeUnmount, onMounted, nextTick, watch, unref, isRef } from 'vue'
-import ScrollingObserver from './scrolling-observer'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-gsap.registerPlugin(ScrollTrigger)
+import deboounce from 'debounce'
 
-function triggerAnimation(options, currentInstance) {
-    const scrollingObserver = new ScrollingObserver()
+class ScrollingObserver {
+    constructor() {
+        if (!ScrollingObserver.instance) {
+            ScrollingObserver.instance = this
 
-    let disposed = false
-    let animationRef
+            this.resizeMap = new Map()
+            this.animationCounter = 1
 
-    function removeMarkers() {
-        if (animationRef && unref(options.markers)) {
-            document.querySelectorAll(".gsap-marker-start, .gsap-marker-end, .gsap-marker-scroller-start, .gsap-marker-scroller-end").forEach(el => {
-                if (new RegExp(animationRef.id + '$').test(el.innerHTML)) {
-                    el.remove()
-                }
-            })
-        }
-    }
-
-    function registerAnimation() {
-        if (disposed) return
-
-        const id = unref(options.name) + '-' + scrollingObserver.animationCounter++
-        animationRef = {
-            id,
-            animation: gsap.timeline({
-                id,
-                scrollTrigger: {
-                    anticipatePin: unref(options.anticipatePin),
-                    end: unref(options.end),
-                    endTrigger: unref(options.endTrigger),
-                    horizontal: unref(options.horizontal),
-                    markers: unref(options.markers),
-                    pin: unref(options.pin),
-                    pinnedContainer: unref(options.pinnedContainer),
-                    pinReparent: unref(options.pinReparent),
-                    pinSpacing: unref(options.pinSpacing),
-                    pinType: unref(options.pinType),
-                    refreshPriority: unref(options.refreshPriority),
-                    scroller: unref(options.scroller),
-                    scrub: unref(options.scrub),
-                    snap: unref(options.snap),
-                    start: unref(options.start),
-                    toggleActions: unref(options.toggleActions),
-                    trigger: unref(options.trigger),
-                    toggleClass: unref(options.toggleClass),
-                    onEnter: unref(options.onEnter), 
-                    onEnterBack: unref(options.onEnterBack), 
-                    onLeave: unref(options.onEnterBack), 
-                    onLeaveBack: unref(options.onLeaveBack), 
-                    onUpdate: unref(options.onUpdate), 
-                    onScrubComplete: unref(options.onScrubComplete), 
-                    onSnapComplete: unref(options.onSnapComplete), 
-                    onToggle: unref(options.onToggle), 
-                }
-            })
-        }
-        const timeline = unref(options.timeline)
-        if (timeline) {
-            animationRef.animation.to(unref(options.trigger), { '--progress': 1, ease: unref(options.ease) }, 0)
-            if (Array.isArray(timeline)) {
-                for(let { target, vars } of timeline) {
-                    animationRef.animation.to(unref(target), vars, 0)
-                }
-            } else {
-                let { target, vars } = timeline
-                animationRef.animation.to(unref(target), vars, 0)
+            if(typeof window !== 'undefined') {
+                this.resizeObserver = new ResizeObserver(deboounce(this.refresh, 100))
             }
         }
 
-        if (animationRef.animation.scrollTrigger.scroller != window) {
-            animationRef.scrollingElement = animationRef.animation.scrollTrigger.scroller
-        } else {
-            animationRef.scrollingElement = window.document.scrollingElement
-        }
-        
-        scrollingObserver.observe(animationRef)
+        return ScrollingObserver.instance
     }
-
-    function destroyAnimation() {
-        if (animationRef) {
-            scrollingObserver.unobserve(animationRef)
-            animationRef.animation.scrollTrigger.kill(false)
-            animationRef.animation.kill()
-
-            removeMarkers()
+    observe(animationRef) {
+        if (!this.resizeMap.has(animationRef.scrollingElement)) {
+            this.resizeMap.set(animationRef.scrollingElement, [])
+            this.resizeObserver.observe(animationRef.scrollingElement)
         }
-        animationRef = null
+         
+        const animationRefs = this.resizeMap.get(animationRef.scrollingElement)
+        animationRefs.push(animationRef)
     }
-
-    async function initAnimation() {
-        await nextTick()
-        destroyAnimation()
-        if (unref(options.enabled)) {
-            await registerAnimation()
+    unobserve(animationRef) {
+        const animationRefs = this.resizeMap.get(animationRef.scrollingElement)
+        const index = animationRefs.indexOf(animationRef)
+        animationRefs.splice(index, 1)
+        if (animationRefs.length == 0) {
+            this.resizeMap.delete(animationRef.scrollingElement)
+            this.resizeObserver.unobserve(animationRef.scrollingElement)
         }
     }
-
-    if(typeof window !== 'undefined') {
-        onBeforeUnmount(() => {
-            destroyAnimation()
-            disposed = true
-        }, currentInstance)
-
-        if(!currentInstance) {
-            onMounted(async () => {
-                await initAnimation()
-                for(let option in options) {
-                    if (isRef(options[option])) {
-                        watch(options[option], () => {
-                            initAnimation() 
-                        })
-                    }
+    refresh(entries) {
+        for (let entry of entries) {
+            const animationRefs = ScrollingObserver.instance.resizeMap.get(entry.target)
+            if (animationRefs) {
+                for (let animationRef of animationRefs) {
+                    animationRef.animation.scrollTrigger.refresh(true)
                 }
-            })
+            }
         }
     }
-
-    return {
-        initAnimation,
-        destroyAnimation
-    }
-
 }
 
-export default triggerAnimation
+export default ScrollingObserver
